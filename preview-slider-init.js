d9c3bdc11199840583b2fb1141ff70ef943380e8
Krassowsky <egorkryazh@gmail.com>
2016-03-10 15:32:38 +0800

    Initial commit

---
+++ preview-slider-init.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(event) {
+
+	previewSliderInit({
+	});
+
+	previewSliderInit({
+		listClass: 'list',
+		listWrapClass: 'listWrap',
+		nextArrowText: '>',
+		prevArrowText: '<'
+	});
+
+
+});
---
+++ preview-slider.js
@@ -0,0 +1,53 @@
+function previewSliderInit(sliderInitOpts) {
+
+	var 
+		listClass = sliderInitOpts.listClass || 'preview-slider',
+		listWrapClass = sliderInitOpts.listWrapClass || 'preview-slider-container',
+		list = document.querySelector('.' + listClass),
+		container = document.querySelector('.' + listWrapClass),
+		items = list.children,
+		itemsNumber = items.length,
+		itemWidth = items[0].offsetWidth,
+		itemsCount = Math.floor(container.clientWidth / itemWidth),
+		itemsOffset = Math.round((container.clientWidth - itemsCount * itemWidth) / (itemsCount + 1));
+
+		itemWidth += itemsOffset;
+		
+		for (var i = 0; i < itemsNumber; i++) {
+			items[i].style.marginLeft = itemsOffset  + 'px';
+		}
+				
+	if(itemsNumber > itemsCount) {
+			
+		var arrowLeft = document.createElement('span'),
+			arrowRight = document.createElement('span'),			
+			listWidth = itemsNumber * itemWidth,
+			step = itemWidth,
+			pos = 0;
+			
+		arrowLeft.className = listClass + '-arrow' + ' ' + listClass + '-arrow-left';
+		arrowRight.className = listClass + '-arrow' + ' ' + listClass + '-arrow-right';
+		arrowLeft.innerHTML = sliderInitOpts.prevArrowText || 'Prev';	
+		arrowRight.innerHTML = sliderInitOpts.nextArrowText || 'Next';
+		container.appendChild(arrowLeft);
+		container.appendChild(arrowRight);
+		list.style.width = listWidth + 'px';
+		list.style.left = 0;
+		
+		arrowRight.addEventListener('click', function(event){
+			if(pos < listWidth - step * itemsCount) {
+			list.style.left = parseInt(list.style.left) - step + 'px';
+			pos += step;
+			}
+		});	
+		arrowLeft.addEventListener('click', function(event){
+			if(pos) { 
+			list.style.left = parseInt(list.style.left) + step + 'px';
+			pos -= step;
+			}
+		});
+	}
+	
+}
+
+
